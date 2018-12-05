257976f14c0a955586de6264014afa41230dd146
rishabhparsuram <rishabhparsuram@gmail.com>
2018-12-04 21:49:56 -0600

    Still haven't gotten anywhere with this. Tried to make progress with Volley library and iporting google maps API but got nowhere.

--- app/src/main/java/com/example/rishabh/tripplanner/MainActivity.java
+++ app/src/main/java/com/example/rishabh/tripplanner/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.rishabh.tripplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     private EditText city;
     private Button touchme;
     private TextView hotel;
@@ -29,6 +43,25 @@ public class MainActivity extends AppCompatActivity {
                     hotel.setText("Please enter a Valid City");
                     restaurant.setText("Please enter a Valid City");
                     weather.setText("Please enter a Valid City");
+                } else {
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                            Request.Method.GET,
+                            "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Urbana,%20IL&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyAwRH6Junl7BV235LakrC3jzV7fCQeEtDg",
+                            null,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(final JSONObject response) {
+                                    //Log.d(TAG, response.toString());
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(final VolleyError error) {
+                            Log.w(TAG, error.toString());
+                        }
+                    });
+                    hotel.setText(jsonObjectRequest.toString());
+                    weather.setText("");
+                    restaurant.setText("");
                 }
             }
         });
