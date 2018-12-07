96adf4a9a7de3fd2dee2d623f4efc8705001a336
rishabhparsuram <rishabhparsuram@gmail.com>
2018-12-06 20:10:19 -0600

    Made some progress. Was having a hard time figuring out how to extract information from JSON array, but eventually it was figured out.

--- app/src/main/java/com/example/rishabh/tripplanner/MainActivity.java
+++ app/src/main/java/com/example/rishabh/tripplanner/MainActivity.java
@@ -17,16 +17,29 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONException;
+//import org.json.simple.*JSONArray;
+//import org.json.simple.*JSONObject;
+//import org.json.simple.parser.*;
+
+import java.io.FileReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private static RequestQueue requestQueue;
     private EditText city;
     private Button touchme;
     private TextView hotel;
     private TextView restaurant;
     private TextView weather;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,22 +49,31 @@ public class MainActivity extends AppCompatActivity {
         weather = (TextView) findViewById(R.id.weather);
         touchme = (Button) findViewById(R.id.enter);
         restaurant = (TextView) findViewById(R.id.FoodPlaces);
+        requestQueue = Volley.newRequestQueue(this);
+
         touchme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (city.getText().toString().length() == 0) {
-                    hotel.setText("Please enter a Valid City");
-                    restaurant.setText("Please enter a Valid City");
-                    weather.setText("Please enter a Valid City");
-                } else {
+                try {
                     JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                             Request.Method.GET,
                             "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Urbana,%20IL&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyAwRH6Junl7BV235LakrC3jzV7fCQeEtDg",
                             null,
                             new Response.Listener<JSONObject>() {
                                 @Override
-                                public void onResponse(final JSONObject response) {
-                                    //Log.d(TAG, response.toString());
+                                public void onResponse(final JSONObject response){
+                                    try{
+                                        JSONObject abc = response.getJSONArray("candidates").getJSONObject(0);
+                                        restaurant.setText(abc.get("geometry").toString());
+                                        Log.d(TAG, abc.get("geometry").toString());
+
+                                    }
+                                    catch (JSONException e) {
+                                        e.printStackTrace();
+
+                                    }
+
+                                    //hotel.setText(response.toString());
                                 }
                             }, new Response.ErrorListener() {
                         @Override
@@ -59,11 +81,41 @@ public class MainActivity extends AppCompatActivity {
                             Log.w(TAG, error.toString());
                         }
                     });
-                    hotel.setText(jsonObjectRequest.toString());
-                    weather.setText("");
-                    restaurant.setText("");
+                    requestQueue.add(jsonObjectRequest);
+                    //hotel.setText(jsonObjectRequest.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    //hotel.setText("Please enter a Valid City");
+                    //restaurant.setText("Please enter a Valid City");
+                    //weather.setText("Please enter a Valid City");
                 }
             }
+
+            ;
+        /*void startAPICall() {
+            try {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.GET,
+                        "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Urbana,%20IL&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyAwRH6Junl7BV235LakrC3jzV7fCQeEtDg",
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(final JSONObject response) {
+                                Log.d(TAG, response.toString());
+                            }
+                            }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(final VolleyError error) {
+                                Log.w(TAG, error.toString());
+                            }
+                        });
+            } catch (Exception e) {
+                e.printStackTrace();
+                hotel.setText("Please enter a Valid City");
+                restaurant.setText("Please enter a Valid City");
+                weather.setText("Please enter a Valid City");
+            }
+        }*/
         });
     }
 }
